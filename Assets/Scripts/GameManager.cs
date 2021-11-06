e6426e7c102042af2b61e23d46a25e77bcaf1b35
weijchen0919 <weijchen0919@gmail.com>
2021-11-06 02:06:14 -0400

    set multiplayer system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,20 +3,93 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+using Debug = UnityEngine.Debug;
 
-public class GameManager : MonoBehaviour
+namespace Team73.Round5.Racing
 {
-    public static GameManager Instance = null;
-
-    private void Awake()
+    public class GameManager : MonoBehaviourPunCallbacks
     {
-        if (Instance != null && Instance != this)
+        public static GameManager Instance = null;
+
+        [Tooltip("Prefab")] 
+        public GameObject playerPrefab;
+
+        private void Awake()
         {
-            Destroy(gameObject);
+            if (Instance != null && Instance != this)
+            {
+                DestroyImmediate(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
         }
-        else
+
+        private void Start()
         {
-            Instance = this;
+            if (playerPrefab == null)
+            {
+                Debug.LogError("playerPrefab is missing, please set in Game Manager");
+            }
+            else
+            {
+                if (PlayerManager.LocalPlayerInstance == null)
+                {
+                    Debug.LogFormat("Instantiate player prefab {0}", SceneManager.GetActiveScene().name);
+                    PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);    
+                }
+                else
+                {
+                    Debug.LogFormat("Ignore scene loading for {0}", SceneManagerHelper.ActiveSceneName);
+                }
+                
+            }
+        }
+
+        public override void OnLeftRoom() {
+            SceneManager.LoadScene(0);
+        }
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+        
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("{0} enter the room", other.NickName);
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("Am I Master Client? {0}", PhotonNetwork.IsMasterClient);
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("{0} leave the room", other.NickName);
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("Am I Master Client? {0}", PhotonNetwork.IsMasterClient);
+                LoadArena();
+            }
+        }
+        
+        private void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("Not master client, no loading scene");
+            }
+
+            Debug.LogFormat("Loading scene for {0} people", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
         }
     }
 }

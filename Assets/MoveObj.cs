654f6f6c3a66cfa4f30f94d981332a27ae00f8df
weijchen0919 <weijchen0919@gmail.com>
2021-11-08 01:26:58 -0500

    add move around function

--- Assets/MoveObj.cs
+++ Assets/MoveObj.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveObj : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float smoothness = 0.01f;
+    [SerializeField] private float duration = 2.0f;
+    [SerializeField] private float offset = 2.0f;
+    [SerializeField] private bool moveHorizontal = true;
+    [SerializeField] private bool startLeft = true;
+    [SerializeField] private bool startUp = true;
+
+    private void Start()
     {
-        
+        MoveAround();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveAround()
     {
+        StartCoroutine(StartMoveAround(smoothness, duration));
+    }
+
+    IEnumerator StartMoveAround(float smoothness, float duration)
+    {
+        float progress = 0f;
+        float step = smoothness / duration;
+        Vector3 offsetVec = Vector3.zero;
+        if (moveHorizontal)
+        {
+            offsetVec = startLeft ? new Vector3(-offset, 0, 0) : new Vector3(offset, 0, 0);
+        }
+        else
+        {
+            offsetVec = startUp ? new Vector3(0, -offset, 0) : new Vector3(0, offset, 0);
+        }
         
+        while (true)
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + offsetVec, progress * Time.deltaTime);
+            progress += step;
+            
+            if (progress >= duration)
+            {
+                progress = 0f;
+                offsetVec *= -1;
+            }
+
+            yield return new WaitForSeconds(smoothness);
+        }
     }
 }
